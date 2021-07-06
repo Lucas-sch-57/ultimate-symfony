cf7da660650946bbcd6514d4d4367a678317aa2c
Lucas <lucas.dev57@outlook.fr>
2021-07-06 15:03:19 +0200

    Ajout du filtre twig amount

---
+++ src/Twig/AmountExtension.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class AmountExtension extends AbstractExtension
+{
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('amount', [$this, 'amount'])
+        ];
+    }
+
+    public function amount($value, string $symbole = '€', string $decsep = ',', string $thousandsep = ' ')
+    {
+        $finalValue = $value / 100;
+        $finalValue = number_format($finalValue, 2, $decsep, $thousandsep);
+        return $finalValue . $symbole;
+    }
+}
