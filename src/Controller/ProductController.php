c077af7f4e9e69c25e8f98bdc96bc5b33747814f
Lucas <lucas.dev57@outlook.fr>
2021-07-06 11:58:25 +0200

    Mise en place des évènements

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -59,9 +59,8 @@ class ProductController extends AbstractController
     /**
      * @Route("/{category_slug}/{slug}", name="product_show", priority = -1)
      */
-    public function show($slug, ProductRepository $productRepository)
+    public function show($slug, $prenom, ProductRepository $productRepository, Request $request)
     {
-
         $product = $productRepository->findOneBy([
             "slug" => $slug,
         ]);
